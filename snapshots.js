24fb47f1dcd97aa94c2ab8a86a6d8a4a40d23c0e
Dániel Kántor <git@daniel-kantor.com>
2020-05-11 00:44:43 +0200

    .

--- snapshots.js
+++ snapshots.js
@@ -12,7 +12,19 @@ const snapshotVerifier = async (cy, text, Cypress) => {
   });
 
   if (currentSnapshot) {
-    return cy.get(`[data-test="${currentSnapshot[text].id}"]`);
+    const dataTestId = currentSnapshot[text].id;
+    const element = cy.get(`[data-test="${dataTestId}"]`);
+
+    element.then($el => {
+      const currentText = $el.text();
+      if (text !== currentText) {
+        console.warn(
+          `String outdated! "[data-test='${dataTestId}']" was referenced using the old string "${text}" but its current string is "${currentText}"`
+        );
+      }
+    });
+
+    return element;
   } else {
     return cy.contains(text).then($el => {
       const newData = {
